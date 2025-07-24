e645e02c6ca299bd4adbf10b86d410d119c38216
samricodev <samuel.isaac.rico.estrada@gmail.com>
2025-07-24 14:16:40 -0600

    fix: bcrypt to bcyptjs

--- src/utils/createAdmin.js
+++ src/utils/createAdmin.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = mongoose.model('User');
 
 async function createAdminUser() {
